ce88da87e08e97b4a2b1d03b87c72fbf778e11d8
Damilare Ajayi <damie.aj95@gmail.com>
2026-01-17 21:15:22 +0100

    add Exercise4 solution

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,72 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Exercise4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input == 0)
+                    break;
+
+                numbers.Add(input);
+            }
+
+            // Core Requirements
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            double average = (double)sum / numbers.Count;
+            int largest = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if (num > largest)
+                    largest = num;
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {largest}");
+
+            // Stretch Challenge - Smallest Positive Number
+            int smallestPositive = int.MaxValue;
+            foreach (int num in numbers)
+            {
+                if (num > 0 && num < smallestPositive)
+                {
+                    smallestPositive = num;
+                }
+            }
+
+            if (smallestPositive == int.MaxValue)
+            {
+                Console.WriteLine("There is no positive number in the list.");
+            }
+            else
+            {
+                Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+            }
+
+            // Stretch Challenge - Sorted List
+            numbers.Sort();
+            Console.WriteLine("The sorted list is:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
     }
 }
